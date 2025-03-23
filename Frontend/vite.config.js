e9ee4834513f6994449320acc119ae1b3689185d
maharsh_nayak <64581149+Maharsh-Nayak@users.noreply.github.com>
2025-03-23 17:58:23 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,13 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-    changeOrigin: true,
-        secure: false,
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:3000',
+  //   },
+  //   changeOrigin: true,
+  //       secure: false,
+  // },
   plugins: [
     react(),
     tailwindcss()
