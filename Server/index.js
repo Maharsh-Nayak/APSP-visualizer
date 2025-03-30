678308a5231773e7274c0439a614b777f4b5cde1
maharsh_nayak <maharshnayak5@gmail.com>
2025-03-30 20:52:11 +0530

    added feedback form

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer';
+import {connect} from 'mongoose';
+import mongoose from "mongoose"
+
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +18,25 @@ app.use(cors({
   origin: "https://apsp-visualizer-frontend.onrender.com",
   credentials: true,
 }));
+
+connect('mongodb+srv://maharshnayak5:hRrrdpfdYFaDfEd9@cluster0.t0bqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+const feedbackSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
 
 app.post('/api/visualize', (req, res) => {
   
@@ -73,6 +96,62 @@ app.post('/api/visualize', (req, res) => {
     res.status(500).send("Invalid matrix format");
   }
 });
+
+app.post("/api/feedback" , async (req, res) => {
+  const {formData} = req.body;
+  console.log(formData);
+
+  const name=formData.name;
+  const email=formData.email;
+  const message=formData.message;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const newFeedback = new Feedback({ name, email, message });
+  try{
+    await newFeedback.save();
+    console.log('Feedback saved to MongoDB');
+  }
+  catch(err){
+    console.log(err);
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',  
+    auth: {
+      user: 'maharshnayak5@gmail.com', 
+      pass: 'zcxz dftu nqpy vnqr'  
+    }
+  });
+
+  const mailOptions = {
+    from: '"PathVisualizer Support" maharshnayak5@gmail.com',
+    to: email,
+    subject: "Feedback Received ✔️",
+    html: `
+      <h2>Thank you for your feedback, ${name}!</h2>
+      <p>We have received your message and appreciate your input.</p>
+      <p><strong>Your Feedback:</strong></p>
+      <blockquote>${message}</blockquote>
+      <p>We will review it and get back to you if necessary.</p>
+      <br>
+      <p>Best regards,</p>
+      <p>The PathVisualizer Team</p>
+    `
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Sent !!!");
+    res.status(200).json({ message: "Feedback submitted and email sent!" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Failed to send email" });
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
